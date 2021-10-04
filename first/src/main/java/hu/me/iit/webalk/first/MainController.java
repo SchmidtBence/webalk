c34028b4d24606546a709c59eeae07feda6c6b63
SchmidtBence <schmidt.bence@student.uni-miskolc.hu>
2021-10-04 11:44:30 +0200

    Órai anyag 2021.10.04

--- first/src/main/java/hu/me/iit/webalk/first/MainController.java
+++ first/src/main/java/hu/me/iit/webalk/first/MainController.java
@@ -1,22 +1,61 @@
 package hu.me.iit.webalk.first;
 
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path="article/")
 public class MainController {
+    private final List<ArticleDto> articles=new ArrayList<>();
 
-    @GetMapping(path="/", produces= MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    ResponseDto gyoker() {
-        ResponseDto responseDto = new ResponseDto();
 
-        responseDto.setId(123);
-        responseDto.setMessage("hello");
-        return responseDto;
+    @GetMapping(path="", produces= MediaType.APPLICATION_JSON_VALUE)
+    List<ArticleDto> allArticles() {
+        return articles;
     }
 
+    @PostMapping(path="")
+            public void newArticle(@Valid @RequestBody ArticleDto articleDto) {
+            articles.add(articleDto);
+            }
+
+    private int findArticleById(String id) {
+            int found = -1;
+            for (int i = 0; i < articles.size(); i++) {
+                if (articles.get(i).getTitle().equals(id)) {
+                    found = i;
+                    break;
+                }
+            }
+        return found;
+        }
+
+        @PutMapping(path = "{id}")
+        public void ReplaceArticle (@PathVariable("id") String id, @Valid @RequestBody ArticleDto articleDto){
+           int found=findArticleById(id);
+
+            if (found != -1) {
+                ArticleDto foundArticle = articles.get(found);
+                foundArticle.setAuthor(articleDto.getAuthor());
+                foundArticle.setPages(articleDto.getPages());
+            }
+        }
+
+    @DeleteMapping(path="{id}")
+    public void DeleteArticle(@PathVariable("id") String id) {
+        int found=-1;
+        for (int i=0; i<articles.size(); i++){
+            if(articles.get(i).getTitle().equals(id)){
+                found=i;
+                break;
+            }
+        }
+        if (found != -1){
+            articles.remove(found);
+        }
+    }
 }
